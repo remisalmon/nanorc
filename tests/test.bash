0871a440d2bae86da0a3fdc6e2cef6332eb81978
Remi Salmon <salmon.remi@gmail.com>
2023-10-06 16:08:16 -0600

    Add bash test

---
+++ tests/test.bash
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+. Test
+
+Test [ Test ]
+
+! Reserved words
+
+Test [[ Reserved words ]] Test
+
+Test $(Command substitution) Test
+
+Test `Command substitution` Test
+
+Test $(( Arithmetic expansion )) Test
+
+Command --options=Test
+
+Test $! Test
+
+Test $variable Test
+
+Test ${variable:-} Test
+
+Test "string" Test
+
+Test 'string' Test
+
+# comment
+
+Test # comment
+
+Test "string # not a comment" Test
+
+bug # comment "string"
