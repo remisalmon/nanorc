b025e409b1fd4a321470bc718c6bc184057cc4a9
Remi Salmon <salmon.remi@gmail.com>
2023-10-26 22:03:45 -0600

    Update python syntax

--- tests/test.py
+++ tests/test.py
@@ -4,6 +4,12 @@ test int() test
 
 test test.int() test
 
+@test
+
+@test()
+
+@test.test()
+
 def test():
 
 def __test__():
@@ -14,35 +20,55 @@ class Test(Test):
 
 test False test
 
-test 1_0.0_0e+0j test
+test 1_0.0_0E+0j test
 
 test BaseException test
 
 test "string" test
 
-test 'string' test
-
-test "" test
+test "\"" test
 
 test "\\" test
 
+test "" test
+
+test r"string" test
+
+test b"string" test
+
+test f"string" test
+
+test rb"string" test
+
+test rf"string" test
+
 test """multiline string""" test
+
+test """multiline
+string""" test
 
 test """
 multiline string
 """ test
 
-test '''multiline string''' test
+test """multiline
+string \"""" test
 
-test '''
-multiline string
-''' test
+test """multiline
+string \\""" test
+
+test """
+""" test
 
 test """""" test
 
 test """""""" test
 
+test """\"""" test
+
 test """\\""" test
+
+test 'string' test
 
 # comment
 
@@ -50,11 +76,11 @@ test # comment
 
 test "string # not a comment" test
 
-test """
-multiline string
+test """multiline
 # not a comment
+string
 """ test
 
-bug # comment "not a string"
+bug # "comment
 
 bug "'''" not a string
