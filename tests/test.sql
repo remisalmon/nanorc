292a20924b049ab7eb9600d3dd41b9b601d2782d
Remi Salmon <salmon.remi@gmail.com>
2024-04-15 22:08:42 -0600

    Update sql syntax

--- tests/test.sql
+++ tests/test.sql
@@ -2,12 +2,21 @@ test select test
 
 test coalesce() test
 
+test "identifier" test
+
 test 'string' test
 
 -- comment
 
 test -- comment
 
-test /*
-comment
+test--comment
+
+test /* comment
 */ test
+
+test
+/*
+comment
+*/
+test
